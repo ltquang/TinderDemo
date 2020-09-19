adf71a677784e93df40c053efa3f916d9bef3aaf
WangLee <ltquangdigi@gmail.com>
2020-09-19 10:59:10 +0700

    add first test case - check feature get user models

--- TinderDemo/TinderDemoTests/TinderDemoTests.swift
+++ TinderDemo/TinderDemoTests/TinderDemoTests.swift
@@ -7,27 +7,42 @@
 //
 
 import XCTest
+import Quick
+import Nimble
+
 @testable import TinderDemo
 
-class TinderDemoTests: XCTestCase {
+class TinderDemoTests: QuickSpec {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    override func spec() {
+        let viewController = ViewController()
+        describe("get user models") {
+            it("get success") {
+                viewController.httpService = HttpServiceMockSuccess()
+                viewController.favoriteManager = FavoriteManager.shared
+                viewController.httpService.getUser { (response) in
+                    if let results = response?["results"] as? [[String: Any]] {
+                        viewController.userModels.removeAll()
+                        for result in results {
+                            viewController.userModels.append(UserModel(json: result))
+                        }
+                        expect(viewController.userModels.count).to(be(1))
+                    }
+                }
+            }
+            it("get fail") {
+                viewController.httpService = HttpServiceMockFail()
+                viewController.favoriteManager = FavoriteManager.shared
+                viewController.httpService.getUser { (response) in
+                    if let results = response?["results"] as? [[String: Any]] {
+                        viewController.userModels.removeAll()
+                        for result in results {
+                            viewController.userModels.append(UserModel(json: result))
+                        }
+                        expect(viewController.userModels.count).to(be(0))
+                    }
+                }
+            }
         }
     }
 }
